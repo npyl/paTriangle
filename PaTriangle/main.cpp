49d8400fd41f43c2d9686549e37ca38a8e0f5cf5
npyl <n.pylarinos@hotmail.com>
2015-07-03 22:51:27 +0300

    Initial Commit

---
+++ PaTriangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PaTriangle
+//
+//  Created by Nickolas Pylarinos on 7/3/15.
+//  Copyright (c) 2015 npyl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
